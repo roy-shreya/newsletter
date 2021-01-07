079f9ddb3be61278056d39f5068110702d4357b2
Shreya <shreya.roy.blr@gmail.com>
2021-01-07 15:00:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const exp = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = exp();
+
+app.use(exp.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", function(req, res){
+    var firstname=req.body.firstName;
+    var lastname = req.body.lastName;
+    var email = req.body.emailAddress;
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            }
+        ]
+    };
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/5f1aad84a4";
+    const options = {
+        method: "POST",
+        auth: "Shreya04:3538507e95282fa71b2424ad046b5b2a-us17"
+    };
+
+    const  request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server running on port 3000");
+});
